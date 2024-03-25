c9d97d6be2d3bf46edb51919dc1f1fce6b765fa0
Kamryn Lightcap <lightcap.13@osu.edu>
2024-03-24 21:58:47 -0400

    Through page 19

--- Blue.Zebra.Api/Program.cs
+++ Blue.Zebra.Api/Program.cs
@@ -1,6 +1,12 @@
+using Blue.Zebra.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options => options.UseSqlite("Data Source= ../Registrar.sqlite", b => b.MigrationsAssembly("Blue.Zebra.Api")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ Blue.Zebra.Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Blue.Zebra.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blue.Zebra.Data
+{
+    public class StoreContext : DbContext
+    {
+    public StoreContext(DbContextOptions<StoreContext> options)
+    : base(options)
+{ }
+public DbSet<Item> Items { get; set; }
+}
+}
+
+
